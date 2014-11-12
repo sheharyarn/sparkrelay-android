1ea312feb499d9ab0d26d479dfd992d6ce4548b7
Sheharyar Naseer <sheharyar92@live.com>
2014-11-13 01:43:52 +0500

    Fragments working

--- mobile/src/main/java/me/sheharyar/sparkrelay/MainActivity.java
+++ mobile/src/main/java/me/sheharyar/sparkrelay/MainActivity.java
@@ -1,7 +1,10 @@
 package me.sheharyar.sparkrelay;
 
 import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.app.FragmentManager;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
@@ -10,8 +13,18 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//
+//        FrameLayout container = (FrameLayout)findViewById(R.id.fragContainer);
 
-
+        try {
+            NoCoresFragment nocores = new NoCoresFragment();
+            FragmentManager frgManager = getFragmentManager();
+            FragmentTransaction trans = frgManager.beginTransaction();
+            trans.replace(R.id.fragContainer, nocores);
+            trans.commit();
+        } catch (Exception e) {
+            Toast.makeText(MainActivity.this, "error " + e.getMessage(), Toast.LENGTH_LONG).show();
+        }
     }
 
 }
--- mobile/src/main/java/me/sheharyar/sparkrelay/NoCoresFragment.java
+++ mobile/src/main/java/me/sheharyar/sparkrelay/NoCoresFragment.java
@@ -18,35 +18,7 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class NoCoresFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     private OnFragmentInteractionListener mListener;
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment NoCoresFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static NoCoresFragment newInstance(String param1, String param2) {
-        NoCoresFragment fragment = new NoCoresFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     public NoCoresFragment() {
         // Required empty public constructor
     }
@@ -54,10 +26,6 @@ public class NoCoresFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -67,23 +35,17 @@ public class NoCoresFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_no_cores, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
 
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        try {
-            mListener = (OnFragmentInteractionListener) activity;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
+//    @Override
+//    public void onAttach(Activity activity) {
+//        super.onAttach(activity);
+//        try {
+//            mListener = (OnFragmentInteractionListener) activity;
+//        } catch (ClassCastException e) {
+//            throw new ClassCastException(activity.toString()
+//                    + " must implement OnFragmentInteractionListener");
+//        }
+//    }
 
     @Override
     public void onDetach() {
